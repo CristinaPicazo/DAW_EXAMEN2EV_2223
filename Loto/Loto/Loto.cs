932e6ac89d9a27630c5c91952ff7a08be712a815
Cristina Picazo Merlos <47087596+CristinaPicazo@users.noreply.github.com>
2023-03-16 19:12:09 +0100

    no me deja hacer push desde VS

--- Loto/Loto/Loto.cs
+++ Loto/Loto/Loto.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace LotoClassNS
 {
@@ -7,77 +7,103 @@ namespace LotoClassNS
     public class loto
     {
         // definición de constantes
+        /// <summary>
+        /// Máximo de números a introducir
+        /// </summary>
         public const int MAX_NUMEROS = 6;
+        /// <summary>
+        /// Número menor que puede salir
+        /// </summary>
         public const int NUMERO_MENOR = 1;
+        /// <summary>
+        /// Número mayor que puede salir
+        /// </summary>
         public const int NUMERO_MAYOR = 49;
-        
-        private int[] _nums = new int[MAX_NUMEROS];   // numeros de la combinación
-        public bool ok = false;      // combinación válida (si es aleatoria, siempre es válida, si no, no tiene porqué)
 
-        public int[] Nums { 
-            get => _nums; 
-            set => _nums = value; 
+        private int[] _listaNumeros = new int[MAX_NUMEROS];   // numeros de la combinación
+        public bool esValida = false;      // combinación válida (si es aleatoria, siempre es válida, si no, no tiene porqué)
+        /// <summary>
+        /// Método público de la lista de números privada
+        /// </summary>
+        public int[] ListaNumeros
+        {
+            get => _listaNumeros;
+            set => _listaNumeros = value;
         }
 
         // En el caso de que el constructor sea vacío, se genera una combinación aleatoria correcta
         //
+        /// <summary>
+        /// Constructor vacío para crear la lotería
+        /// </summary>
         public loto()
         {
             Random r = new Random();    // clase generadora de números aleatorios
 
-            int i=0, j, num;
+            int i = 0, j, numeroAleatorio;
 
             do             // generamos la combinación
-            {                       
-                num = r.Next(NUMERO_MENOR, NUMERO_MAYOR + 1);     // generamos un número aleatorio del 1 al 49
-                for (j=0; j<i; j++)    // comprobamos que el número no está
-                    if (Nums[j]==num)
+            {
+                numeroAleatorio = r.Next(NUMERO_MENOR, NUMERO_MAYOR + 1);     // generamos un número aleatorio del 1 al 49
+                for(j = 0; j < i; j++)    // comprobamos que el número no está
+                    if(ListaNumeros[j] == numeroAleatorio)
                         break;
-                if (i==j)               // Si i==j, el número no se ha encontrado en la lista, lo añadimos
+                if(i == j)               // Si i==j, el número no se ha encontrado en la lista, lo añadimos
                 {
-                    Nums[i]=num;
+                    ListaNumeros[i] = numeroAleatorio;
                     i++;
                 }
-            } while (i<MAX_NUMEROS);
+            } while(i < MAX_NUMEROS);
 
-            ok=true;
+            esValida = true;
         }
 
         // La segunda forma de crear una combinación es pasando el conjunto de números
         // misnums es un array de enteros con la combinación que quiero crear (no tiene porqué ser válida)
-        public loto(int[] misnums)  // misnumeros: combinación con la que queremos inicializar la clase
+        /// <summary>
+        /// Constructor con parametro para crear la lotería
+        /// </summary>
+        /// <param name="misnumeros"></param>
+        public loto(int[] misnumeros)  // misnumeros: combinación con la que queremos inicializar la clase
         {
-            for (int i=0; i<MAX_NUMEROS; i++)
-                if (misnums[i]>=NUMERO_MENOR && misnums[i]<=NUMERO_MAYOR) {
+            for(int i = 0; i < MAX_NUMEROS; i++)
+                if(misnumeros[i] >= NUMERO_MENOR && misnumeros[i] <= NUMERO_MAYOR)
+                {
                     int j;
-                    for (j=0; j<i; j++) 
-                        if (misnums[i]==Nums[j])
+                    for(j = 0; j < i; j++)
+                        if(misnumeros[i] == ListaNumeros[j])
                             break;
-                    if (i==j)
-                        Nums[i]=misnums[i]; // validamos la combinación
-                    else {
-                        ok=false;
+                    if(i == j)
+                        ListaNumeros[i] = misnumeros[i]; // validamos la combinación
+                    else
+                    {
+                        esValida = false;
                         return;
                     }
                 }
                 else
                 {
-                    ok=false;     // La combinación no es válida, terminamos
+                    esValida = false;     // La combinación no es válida, terminamos
                     return;
                 }
-	    ok=true;
+            esValida = true;
         }
 
         // Método que comprueba el número de aciertos
         // premi es un array con la combinación ganadora
         // se devuelve el número de aciertos
-        public int comprobar(int[] premi)
+        /// <summary>
+        /// Método con parámetro para comprobar el número de aciertos
+        /// </summary>
+        /// <param name="listaNumerosPremiados"></param>
+        /// <returns></returns>
+        public int comprobar(int[] listaNumerosPremiados)
         {
-            int a=0;                    // número de aciertos
-            for (int i=0; i<MAX_NUMEROS; i++)
-                for (int j=0; j<MAX_NUMEROS; j++)
-                    if (premi[i]==Nums[j]) a++;
-            return a;
+            int numeroAciertos = 0;                    // número de aciertos
+            for(int i = 0; i < MAX_NUMEROS; i++)
+                for(int j = 0; j < MAX_NUMEROS; j++)
+                    if(listaNumerosPremiados[i] == ListaNumeros[j]) numeroAciertos++;
+            return numeroAciertos;
         }
     }
 
